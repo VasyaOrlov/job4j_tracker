4b599bd47cedf190383aa8ad6eaf5e4ea314678b
VasiliiOrlov <ov_prog@mail.ru>
2021-06-01 10:54:17 +0300

    Взаимодействие объектов [#166087 #8847]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void goAwayWolf() {
+        System.out.println("не ешь меня!");
+    }
+
+    public void goAwayHare() {
+        System.out.println("не ешь меня!");
+    }
+
+    public void dieOfFox() {
+        System.out.println("не ешь меня!");
+        System.out.println("аааааааа");
+    }
+}
