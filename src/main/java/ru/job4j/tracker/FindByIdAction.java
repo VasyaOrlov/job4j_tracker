818388d55302655f4dd525c58bb367dff60868a0
VasiliiOrlov <ov_prog@mail.ru>
2021-06-05 15:23:33 +0300

    Зависимость от System.out [#33568 #8853]

--- src/main/java/ru/job4j/tracker/FindByIdAction.java
+++ src/main/java/ru/job4j/tracker/FindByIdAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class FindByIdAction implements UserAction {
+    private final Output out;
+
+    public FindByIdAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Find item by id";
@@ -8,7 +14,7 @@ public class FindByIdAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Find item by id ====");
+        out.println("=== Find item by id ====");
         int id = Integer.parseInt(input.askStr("Enter id: "));
         Item item = tracker.findById(id);
         if (item != null) {
