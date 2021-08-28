135b1fca7e62b061bdb2679421d7c42eb6e4be01
VasiliiOrlov <ov_prog@mail.ru>
2021-08-28 11:30:08 +0300

    Лямбда блок [#249208] fix1

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,16 +1,21 @@
 package ru.job4j.lambda;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class LambdaUsage {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("Zina", "Ken", "Sasha", "Petr"));
         Comparator<String> cmpText = (left, right) -> {
             System.out.println("compare - " + right + " : " + left);
             return right.compareTo(left);
         };
+        list.sort(cmpText);
         Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + right.length() + " : " + left.length());
             return Integer.compare(right.length(), left.length());
         };
+        list.sort(cmpDescSize);
     }
 }
