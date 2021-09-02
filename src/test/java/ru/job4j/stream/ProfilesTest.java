cc07eedfff3b6ca02bb29dabeef64621b76d533b
VasiliiOrlov <ov_prog@mail.ru>
2021-09-02 15:54:02 +0300

    Уникальность элементов и сортировка. [#110225 #8918]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -10,12 +10,15 @@ public class ProfilesTest {
     public void collectTest() {
         List<Profile> list = List.of(
                 new Profile(new Address("Moscow", "Krasnay", 1, 1)),
+                new Profile(new Address("Kirov", "Krasnay", 1, 1)),
                 new Profile(new Address("Moscow", "Krasnay", 1, 2)),
+                new Profile(new Address("Moscow", "Krasnay", 1, 1)),
                 new Profile(new Address("Moscow", "Krasnay", 1, 3))
                 );
         Profiles profiles = new Profiles();
         List<Address> rsl = profiles.collect(list);
         List<Address> expect = List.of(
+                new Address("Kirov", "Krasnay", 1, 1),
                 new Address("Moscow", "Krasnay", 1, 1),
                 new Address("Moscow", "Krasnay", 1, 2),
                 new Address("Moscow", "Krasnay", 1, 3)
